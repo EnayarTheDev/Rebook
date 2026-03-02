422f0b2f9ac07c2fae308ba9f739b09a422f7cd7
EnayarTheDev <enayarwastaken@gmail.com>
2026-03-02 00:16:05 +0000

    restore matcher

--- proxy.ts
+++ proxy.ts
@@ -5,5 +5,7 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
